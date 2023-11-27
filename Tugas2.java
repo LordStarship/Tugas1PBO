284b38d588151f94f2e7542ec051dd7d4a5d3247
LordStarship <lordstarship.boat@gmail.com>
2023-11-27 20:50:23 +0700

    Tugas 2

---
+++ Tugas2.java
@@ -0,0 +1,17 @@
+package com.mycompany.tugaspbo;
+import java.io.FileNotFoundException;
+
+public class Tugas2 {
+    public static void main(String[] args){
+        try {
+            readFileTugas2 readFile1 = new readFileTugas2();
+            readFile1.bacaFile("ename.txt");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File tidak ditemukan!");
+        }
+        catch (Exception e) {
+            System.out.println("Tidak dapat membuka file!");
+        }
+    }
+}
---
+++ readFileTugas2.java
@@ -0,0 +1,20 @@
+package com.mycompany.tugaspbo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class readFileTugas2 {
+    
+    public void bacaFile(String fileName) throws FileNotFoundException{
+        File myFile = new File(fileName);
+        myFile.exists();
+        
+        Scanner readFile = new Scanner(myFile);
+        throw new FileNotFoundException("File tidak ditemukan!");
+    }
+}
